7da7a013b64139ca1bab186707f80eab0d80dfce
sika0819 <740015523@qq.com>
2018-04-20 10:11:38 +0800

    提交修改
    删除不必要的东西，增加新立绘什么的

--- Assets/Script/Scripter.cs
+++ Assets/Script/Scripter.cs
@@ -1,15 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
 [System.Serializable]
 public class Scripter : Singleton<Scripter> {
-    string DBName = "avg";
 	void Awake()
 	{
-        DontDestroyOnLoad(gameObject);
-        string path = "URI=file:" + Application.streamingAssetsPath + "/" + DBName + ".db";
-        DB.Instance.Open(path);
+     
 	}
 	// Use this for initialization
 	void Start () {
